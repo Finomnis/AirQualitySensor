e1cee7bb9488df30f756130c0b68559e0d06a2c5
Finomnis <finomnis@gmail.com>
2021-05-19 01:05:42 +0200

    Fix ESP8266 pinout for alternate UART0

--- src/pinout.hpp
+++ src/pinout.hpp
@@ -28,6 +28,14 @@ namespace PINS
         Serial.begin(9600);
 
 #ifdef ESP8266
+        // Swap the default serial to alternate pins
+        // TX: GPIO15
+        // RX: GPIO13
+        // Further, connect GPIO15 to GPIO01 (TXD0) with a 10k resistor
+        // to forward logging info back to UART.
+        // Then, connect S8LP to GPIO13 and GPIO2
+        Serial.swap();
+
         Serial1.begin(9600);
 #endif
 #ifdef ESP32
