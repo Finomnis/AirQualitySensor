dc15507ac2795e78893044ecdfcf9e31ba359697
Finomnis <finomnis@gmail.com>
2021-05-22 01:08:05 +0200

    First working version of modbus rpc

--- src/sensor_S8LP_modbusRPC.cpp
+++ src/sensor_S8LP_modbusRPC.cpp
@@ -26,6 +26,29 @@ namespace
             }
         }
     }
+
+    bool validate_crc(uint8_t *data, size_t len)
+    {
+        uint16_t crc = initCRC();
+
+        uint16_t actual_crc = (uint16_t(data[len - 1]) << 8) + uint16_t(data[len - 2]);
+
+        for (size_t i = 0; i < len - 2; i++)
+        {
+            updateCRC(crc, data[i]);
+        }
+
+        if (crc != actual_crc)
+        {
+            Homie.getLogger().print(F("SensorS8LP: Error: CRC didn't match: "));
+            Homie.getLogger().print(actual_crc, HEX);
+            Homie.getLogger().print(F(" != "));
+            Homie.getLogger().println(crc, HEX);
+            return false;
+        }
+
+        return true;
+    }
 }
 
 SensorS8LP_ModbusRPC::SensorS8LP_ModbusRPC(HardwareSerial *sender, HardwareSerial *receiver,
@@ -33,14 +56,43 @@ SensorS8LP_ModbusRPC::SensorS8LP_ModbusRPC(HardwareSerial *sender, HardwareSeria
                                            size_t response_size)
     : _function_code{function_code},
       _response_size{response_size},
+      _response_payload_size{response_size + 2},
       _sender{sender},
       _receiver{receiver}
 {
+    if (_response_payload_size + 2 > MESSAGE_MAX_SIZE)
+    {
+        Homie.getLogger().println(F("ERROR: expected response size is too large!"));
+        finish_error();
+        return;
+    }
+
     if (!sendModbusRequest(function_code, request_data, request_size))
     {
-        _finished = true;
-        _success = false;
+        finish_error();
+        return;
     }
+}
+
+void SensorS8LP_ModbusRPC::wait_for_finished()
+{
+    while (!_finished)
+    {
+        update();
+    }
+}
+
+void SensorS8LP_ModbusRPC::finish_error(uint8_t error_code)
+{
+    _finished = true;
+    _success = false;
+    _error_code = error_code;
+}
+
+void SensorS8LP_ModbusRPC::finish_success()
+{
+    _finished = true;
+    _success = true;
 }
 
 void SensorS8LP_ModbusRPC::update()
@@ -49,6 +101,8 @@ void SensorS8LP_ModbusRPC::update()
     {
         return;
     }
+
+    updateReceiveModbusResponse();
 }
 
 bool SensorS8LP_ModbusRPC::sendModbusRequest(uint8_t function_code, const uint8_t *data, size_t data_len)
@@ -98,18 +152,86 @@ bool SensorS8LP_ModbusRPC::sendModbusRequest(uint8_t function_code, const uint8_
     return true;
 }
 
+void SensorS8LP_ModbusRPC::updateReceiveModbusResponse()
+{
+    bool new_data_received = false;
+
+    while (_receiver->available() > 0)
+    {
+        if (_response_num_received >= MESSAGE_MAX_SIZE)
+        {
+            finish_error();
+            return;
+        }
+
+        int data = _receiver->read();
+        if (data < 0)
+        {
+            finish_error();
+            return;
+        }
+
+        _response[_response_num_received++] = data;
+        new_data_received = true;
+    }
+
+    if (!new_data_received)
+    {
+        return;
+    }
+
+    if (_response_num_received < 5)
+        return;
+
+    uint8_t address = _response[0];
+    if (address != uint8_t('\xfe'))
+    {
+        Homie.getLogger().print(F("SensorS8LP: Error: Invalid message address: "));
+        Homie.getLogger().println(address, HEX);
+        finish_error();
+        return;
+    }
+
+    uint8_t actual_function_code = _response[1];
+
+    // Error
+    if (actual_function_code == (_function_code | uint8_t(0x80)))
+    {
+        if (!validate_crc(_response, 5))
+        {
+            finish_error();
+            return;
+        }
+
+        finish_error(_response[2]);
+        return;
+    }
+
+    if (actual_function_code != _function_code)
+    {
+        Homie.getLogger().print(F("SensorS8LP: Error: Function code didn't match: "));
+        Homie.getLogger().print(actual_function_code, HEX);
+        Homie.getLogger().print(F(" != "));
+        Homie.getLogger().println(_function_code, HEX);
+        finish_error();
+        return;
+    }
+
+    if (_response_num_received < _response_payload_size + 2)
+        return;
+
+    if (!validate_crc(_response, _response_payload_size + 2))
+    {
+        finish_error();
+        return;
+    }
+
+    finish_success();
+    return;
+}
+
 // bool SensorS8LP::receiveModbusResponse(uint8_t function_code, uint8_t *data, size_t data_len)
 // {
-//     constexpr size_t PAYLOAD_MAX_SIZE = MESSAGE_MAX_SIZE - 2;
-//     uint8_t payload[PAYLOAD_MAX_SIZE];
-//     size_t payload_len = data_len + 2;
-
-//     if (payload_len > PAYLOAD_MAX_SIZE)
-//     {
-//         Homie.getLogger().println(F("ERROR: receiveModbusResponse data was too large!"));
-//         return false;
-//     }
-
 //     uint8_t address;
 //     if (1 != receiver->readBytes(&address, 1))
 //     {
--- src/sensor_S8LP_modbusRPC.hpp
+++ src/sensor_S8LP_modbusRPC.hpp
@@ -12,13 +12,21 @@ public:
                          uint8_t function_code, uint8_t *request_data, size_t request_size,
                          size_t response_size);
     void update();
+    void wait_for_finished();
+    bool finished() { return _finished; }
+    bool success() { return _success; }
+    const uint8_t *response_payload() { return &_response[2]; }
 
 private:
     bool sendModbusRequest(uint8_t function_code, const uint8_t *data, size_t data_len);
+    void updateReceiveModbusResponse();
+    void finish_error(uint8_t error_code = 0x0);
+    void finish_success();
 
 private: // Constant values determined at constructor time
     const uint8_t _function_code;
     const size_t _response_size;
+    const size_t _response_payload_size;
     HardwareSerial *const _sender;
     HardwareSerial *const _receiver;
 
@@ -29,5 +37,5 @@ private: // Values that reflect the result state
 
 private: // Internal state required to receive non-blocking
     uint8_t _response[MESSAGE_MAX_SIZE];
-    size_t _response_size{0};
+    size_t _response_num_received{0};
 };
