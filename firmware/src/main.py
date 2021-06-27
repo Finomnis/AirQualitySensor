9704dcd375f0c7db22f42db185c24e36856926d2
Finomnis <finomnis@gmail.com>
2021-06-27 16:47:48 +0200

    Add status LEDs based on CO2 value

--- firmware/src/main.py
+++ firmware/src/main.py
@@ -6,6 +6,7 @@ import pins
 
 from modules.DHT22 import DHT22
 from modules.SensairS8 import SensairS8
+from modules.LEDs import LEDs
 
 
 async def main():
@@ -15,6 +16,10 @@ async def main():
     sensor_dht22 = DHT22(id="dht22", name="DHT22", pin=pins.DHT22, interval=2)
     sensor_s8lp = SensairS8(id="s8lp", name="S8LP", interval=4)
 
+    leds = LEDs()
+
+    sensor_s8lp.register_co2_handler(leds.on_co2_changed)
+
     device.add_node(sensor_dht22)
     device.add_node(sensor_s8lp)
 
@@ -23,6 +28,7 @@ async def main():
         device.run(),
         sensor_dht22.run(),
         sensor_s8lp.run(),
+        leds.run(),
     )
 
 
