fec154ba48fda725d799dba8fa849e6598a5a237
Finomnis <finomnis@gmail.com>
2021-06-27 13:05:40 +0200

    Add Waveshare 15.4 inch Eink display module

---
+++ firmware/src/drivers/waveshare/epaper1in54_async.py
@@ -0,0 +1,147 @@
+from micropython import const
+from machine import SPI, Pin
+import utime
+
+import uasyncio as asyncio
+
+CMD_DRIVER_OUTPUT_CONTROL = const(0x01)
+CMD_DATA_ENTRY_MODE_SETTING = const(0x11)
+CMD_SW_RESET = const(0x12)
+CMD_MASTER_ACTIVATION = const(0x20)
+CMD_DISPLAY_UPDATE_CONTROL_2 = const(0x22)
+CMD_WRITE_RAM_BLACK = const(0x24)
+CMD_WRITE_RAM_COLOR = const(0x26)
+CMD_SET_RAM_X_ADDRESS_START_END = const(0x44)
+CMD_SET_RAM_Y_ADDRESS_START_END = const(0x45)
+CMD_SET_RAM_X_ADDRESS_COUNTER = const(0x4E)
+CMD_SET_RAM_Y_ADDRESS_COUNTER = const(0x4F)
+
+# Display resolution
+EPD_WIDTH = const(200)
+EPD_HEIGHT = const(200)
+
+
+class EPaper:
+
+    def __init__(self, spi: SPI, cs: Pin, dc: Pin, rst: Pin, busy: Pin):
+        self.spi = spi
+        self.cs = cs
+        self.dc = dc
+        self.rst = rst
+        self.busy = busy
+        self.cs.init(self.cs.OUT, value=1)
+        self.dc.init(self.dc.OUT, value=1)
+        self.rst.init(self.rst.OUT, value=1)
+        self.busy.init(self.busy.IN)
+
+        self.width = EPD_WIDTH
+        self.height = EPD_HEIGHT
+
+    async def init(self):
+        # Reset
+        self._reset()
+
+        # Basic init
+        self._write_spi(
+            CMD_DRIVER_OUTPUT_CONTROL,
+            bytes([
+                (EPD_HEIGHT - 1) & 0xFF,
+                ((EPD_HEIGHT - 1) >> 8) & 0xFF,
+                0x00
+            ])
+        )
+
+        # Data entry sequence setting
+        self._write_spi(
+            CMD_DATA_ENTRY_MODE_SETTING,
+            bytes([0x11])
+        )
+
+        # Set RAMX Start/end position
+        self._write_spi(
+            CMD_SET_RAM_X_ADDRESS_START_END,
+            bytes([0x00, 0x18])
+        )
+
+        # Set RAMY Start/end position
+        self._write_spi(
+            CMD_SET_RAM_Y_ADDRESS_START_END,
+            bytes([0x00, 0x00, 0xC7, 0x00])
+        )
+
+        # Load operation LUT
+        self._write_spi(
+            CMD_DISPLAY_UPDATE_CONTROL_2,
+            bytes([0xb9])
+        )
+
+        # Execute operation
+        self._write_spi(
+            CMD_MASTER_ACTIVATION
+        )
+
+        await self.wait_until_idle()
+
+    def _write_spi(self, command: int, data=None):
+        self.dc(0)
+        self.cs(0)
+        self.spi.write(bytes([command]))
+        if data is not None:
+            self.dc(1)
+            self.spi.write(data)
+            self.dc(0)
+        self.cs(1)
+
+    def _reset(self):
+        # Panel reset
+        self.rst(0)
+        utime.sleep_ms(10)
+        self.rst(1)
+        utime.sleep_ms(10)
+        # Software reset
+        self._write_spi(CMD_SW_RESET)
+
+    async def wait_until_idle(self):
+        while self.busy():
+            await asyncio.sleep_ms(1)
+
+    async def display_frame(self, data_bw, data_col=None):
+        self._write_spi(
+            CMD_SET_RAM_X_ADDRESS_COUNTER,
+            bytes([0x00])
+        )
+        self._write_spi(
+            CMD_SET_RAM_Y_ADDRESS_COUNTER,
+            bytes([0x00, 0x00])
+        )
+        self._write_spi(
+            CMD_WRITE_RAM_BLACK,
+            data_bw
+        )
+
+        if data_col is not None:
+            self._write_spi(
+                CMD_SET_RAM_X_ADDRESS_COUNTER,
+                bytes([0x00])
+            )
+            self._write_spi(
+                CMD_SET_RAM_Y_ADDRESS_COUNTER,
+                bytes([0x00, 0x00])
+            )
+            self._write_spi(
+                CMD_WRITE_RAM_COLOR,
+                data_col
+            )
+
+        # Load operation LUT
+        self._write_spi(
+            CMD_DISPLAY_UPDATE_CONTROL_2,
+            bytes([0xF7])
+        )
+
+        # Execute operation
+        self._write_spi(
+            CMD_MASTER_ACTIVATION
+        )
+
+        await self.wait_until_idle()
--- firmware/src/main.py
+++ firmware/src/main.py
@@ -4,8 +4,9 @@ import uasyncio as asyncio
 import settings
 import pins
 
-from sensors.DHT22 import DHT22
-from sensors.SensairS8 import SensairS8
+from modules.DHT22 import DHT22
+from modules.SensairS8 import SensairS8
+from modules.Waveshare154 import Waveshare154
 
 
 async def main():
@@ -14,6 +15,9 @@ async def main():
 
     sensor_dht22 = DHT22(id="dht22", name="DHT22", pin=pins.DHT22, interval=2)
     sensor_s8lp = SensairS8(id="s8lp", name="S8LP", interval=4)
+    display_eink = Waveshare154()
+
+    await display_eink.init()
 
     device.add_node(sensor_dht22)
     device.add_node(sensor_s8lp)
@@ -23,6 +27,7 @@ async def main():
         device.run(),
         sensor_dht22.run(),
         sensor_s8lp.run(),
+        display_eink.run(),
     )
 
 
