c05af4ad2d678a52b690373505f721e23ccb7d9a
Finomnis <finomnis@gmail.com>
2021-07-19 02:17:30 +0200

    Inverted order of device id

--- firmware/src/homie/DeviceId.cpp
+++ firmware/src/homie/DeviceId.cpp
@@ -10,7 +10,13 @@ namespace
         Identifier()
         {
             uint64_t chipid = ESP.getEfuseMac() & 0xffffffffffff;
-            snprintf(value, sizeof(value), "air-sensor-%012llx", chipid);
+            snprintf(value, sizeof(value), "air-sensor-%02x%02x%02x%02x%02x%02x",
+                     uint8_t((chipid >> 0) & 0xff),
+                     uint8_t((chipid >> 8) & 0xff),
+                     uint8_t((chipid >> 16) & 0xff),
+                     uint8_t((chipid >> 24) & 0xff),
+                     uint8_t((chipid >> 32) & 0xff),
+                     uint8_t((chipid >> 40) & 0xff));
         }
         static constexpr size_t ID_MAX_LEN = 128;
         char value[ID_MAX_LEN];
