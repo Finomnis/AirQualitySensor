f04e8dd8ec3d4f6c5c8e04a27788199b26fe6b8a
Finomnis <finomnis@gmail.com>
2022-02-06 17:02:36 +0100

    Adjust temperature offset

--- firmware_rust/AirQualitySensor/src/subsystems/sensor.rs
+++ firmware_rust/AirQualitySensor/src/subsystems/sensor.rs
@@ -89,7 +89,7 @@ impl Sensor {
 
             // TODO: Adjust calibration values if necessary
             controller
-                .set_temperature_offset(9.2)
+                .set_temperature_offset(10.0)
                 .error_message("Unable to set temperature offset")?;
             controller
                 .set_sensor_altitude(320)
